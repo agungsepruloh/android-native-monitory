1431c19010518c6eaa0677456c479c71e3e33805
Agung Sepruloh <agungsepruloh1996@gmail.com>
2021-04-30 08:36:36 +0700

    Enabled Data Binding and create HomeFragment

---
+++ app/src/main/java/com/agung/latihan3monitory/HomeFragment.kt
@@ -0,0 +1,25 @@
+package com.agung.latihan3monitory
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.agung.latihan3monitory.databinding.FragmentHomeBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HomeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentHomeBinding>(
+            inflater, R.layout.fragment_home, container, false)
+        return binding.root
+    }
+}
--- app/src/main/java/com/agung/latihan3monitory/MainActivity.kt
+++ app/src/main/java/com/agung/latihan3monitory/MainActivity.kt
@@ -2,10 +2,13 @@ package com.agung.latihan3monitory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.agung.latihan3monitory.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
