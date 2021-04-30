811764d7a4eed2113905b196a70b4245fbe95324
Agung Sepruloh <agungsepruloh1996@gmail.com>
2021-04-30 09:01:13 +0700

    Rename HomeFragment to WelcomeFragment

--- app/src/main/java/com/agung/latihan3monitory/WelcomeFragment.kt
+++ app/src/main/java/com/agung/latihan3monitory/WelcomeFragment.kt
@@ -6,20 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import com.agung.latihan3monitory.databinding.FragmentHomeBinding
+import com.agung.latihan3monitory.databinding.FragmentWelcomeBinding
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
+ * Use the [WelcomeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment() {
+class WelcomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = DataBindingUtil.inflate<FragmentHomeBinding>(
-            inflater, R.layout.fragment_home, container, false)
+        val binding = DataBindingUtil.inflate<FragmentWelcomeBinding>(
+            inflater, R.layout.fragment_welcome, container, false)
         return binding.root
     }
 }
